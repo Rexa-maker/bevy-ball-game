a8534877bfa3028725c6495ddc62feb80a765808
Le Ax <axelsoot@gmail.com>
2023-04-02 16:41:01 -0700

    ep 9

--- src/game/score/mod.rs
+++ src/game/score/mod.rs
@@ -15,8 +15,6 @@ impl Plugin for ScorePlugin {
             .add_system(insert_score.in_schedule(OnEnter(AppState::Game)))
             .add_system(remove_score.in_schedule(OnExit(AppState::Game)))
             .add_system(update_score.run_if(in_state(AppState::Game)))
-            .add_systems(
-                (update_high_scores, high_scores_updated).in_set(OnUpdate(AppState::GameOver)),
-            );
+            .add_systems((update_high_scores, high_scores_updated));
     }
 }
--- src/systems.rs
+++ src/systems.rs
@@ -49,8 +49,10 @@ pub fn exit_game(
     }
 }
 
-pub fn handle_game_over(mut game_over_event_reader: EventReader<GameOver>) {
+pub fn handle_game_over(mut commands: Commands, mut game_over_event_reader: EventReader<GameOver>) {
     for event in game_over_event_reader.iter() {
         println!("Your final score is: {}", event.score.to_string());
+        commands.insert_resource(NextState(Some(AppState::GameOver)));
+        commands.insert_resource(NextState(Some(SimulationState::Paused)));
     }
 }
