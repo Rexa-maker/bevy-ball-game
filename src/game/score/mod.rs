b7116922c362fe7c170b9538141ad6eb523cd36d
Le Ax <axelsoot@gmail.com>
2023-04-02 16:32:45 -0700

    add/remove score resource instead of resetting it

--- src/game/score/mod.rs
+++ src/game/score/mod.rs
@@ -2,7 +2,7 @@ pub mod resources;
 mod systems;
 
 use bevy::prelude::*;
-use resources::{HighScores, Score};
+use resources::HighScores;
 use systems::*;
 
 use crate::AppState;
@@ -11,10 +11,10 @@ pub struct ScorePlugin;
 
 impl Plugin for ScorePlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<Score>()
-            .init_resource::<HighScores>()
+        app.init_resource::<HighScores>()
+            .add_system(insert_score.in_schedule(OnEnter(AppState::Game)))
+            .add_system(remove_score.in_schedule(OnExit(AppState::Game)))
             .add_system(update_score.run_if(in_state(AppState::Game)))
-            .add_system(reset_score.in_schedule(OnExit(AppState::Game)))
             .add_systems(
                 (update_high_scores, high_scores_updated).in_set(OnUpdate(AppState::GameOver)),
             );
