cc63d82698b16e112200dcbe27be716f1a6595eb
Le Ax <axelsoot@gmail.com>
2023-04-02 17:27:26 -0700

    use NextState.set instead of Commands.insert_resource when changing state

--- src/game/systems.rs
+++ src/game/systems.rs
@@ -3,17 +3,17 @@ use bevy::prelude::*;
 use super::SimulationState;
 
 pub fn toggle_simulation(
-    mut commands: Commands,
     keyboard_input: Res<Input<KeyCode>>,
     simulation_state: Res<State<SimulationState>>,
+    mut next_simulation_state: ResMut<NextState<SimulationState>>,
 ) {
     if keyboard_input.just_pressed(KeyCode::Space) {
         match simulation_state.0 {
             SimulationState::Running => {
-                commands.insert_resource(NextState(Some(SimulationState::Paused)));
+                next_simulation_state.set(SimulationState::Paused);
             }
             SimulationState::Paused => {
-                commands.insert_resource(NextState(Some(SimulationState::Running)));
+                next_simulation_state.set(SimulationState::Running);
             }
         }
     }
--- src/systems.rs
+++ src/systems.rs
@@ -16,26 +16,29 @@ pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Pr
 }
 
 pub fn transition_to_game_state(
-    mut commands: Commands,
     keyboard_input: Res<Input<KeyCode>>,
     app_state: Res<State<AppState>>,
+    mut next_app_state: ResMut<NextState<AppState>>,
+    mut next_simulation_state: ResMut<NextState<SimulationState>>,
 ) {
     if keyboard_input.just_pressed(KeyCode::G) {
         if app_state.0 != AppState::Game {
-            commands.insert_resource(NextState(Some(AppState::Game)));
+            next_app_state.set(AppState::Game);
+            next_simulation_state.set(SimulationState::Paused);
         }
     }
 }
 
 pub fn transition_to_main_menu_state(
-    mut commands: Commands,
     keyboard_input: Res<Input<KeyCode>>,
     app_state: Res<State<AppState>>,
+    mut next_app_state: ResMut<NextState<AppState>>,
+    mut next_simulation_state: ResMut<NextState<SimulationState>>,
 ) {
     if keyboard_input.just_pressed(KeyCode::M) {
         if app_state.0 != AppState::MainMenu {
-            commands.insert_resource(NextState(Some(SimulationState::Paused)));
-            commands.insert_resource(NextState(Some(AppState::MainMenu)));
+            next_app_state.set(AppState::MainMenu);
+            next_simulation_state.set(SimulationState::Paused);
         }
     }
 }
@@ -49,10 +52,8 @@ pub fn exit_game(
     }
 }
 
-pub fn handle_game_over(mut commands: Commands, mut game_over_event_reader: EventReader<GameOver>) {
+pub fn handle_game_over(mut game_over_event_reader: EventReader<GameOver>) {
     for event in game_over_event_reader.iter() {
         println!("Your final score is: {}", event.score.to_string());
-        commands.insert_resource(NextState(Some(AppState::GameOver)));
-        commands.insert_resource(NextState(Some(SimulationState::Paused)));
     }
 }
