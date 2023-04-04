740d5a18bc0be1d5b8ceaf58fad1ecec4a848851
Le Ax <axelsoot@gmail.com>
2023-04-03 18:06:12 -0700

    ep10 start

--- src/game/systems.rs
+++ src/game/systems.rs
@@ -2,18 +2,28 @@ use bevy::prelude::*;
 
 use super::SimulationState;
 
+pub fn pause_simulation(mut next_simulation_state: ResMut<NextState<SimulationState>>) {
+    next_simulation_state.set(SimulationState::Paused);
+    println!("Simulation paused.");
+}
+
+pub fn resume_simulation(mut next_simulation_state: ResMut<NextState<SimulationState>>) {
+    next_simulation_state.set(SimulationState::Running);
+    println!("Simulation resumed.");
+}
+
 pub fn toggle_simulation(
     keyboard_input: Res<Input<KeyCode>>,
     simulation_state: Res<State<SimulationState>>,
-    mut next_simulation_state: ResMut<NextState<SimulationState>>,
+    next_simulation_state: ResMut<NextState<SimulationState>>,
 ) {
     if keyboard_input.just_pressed(KeyCode::Space) {
         match simulation_state.0 {
             SimulationState::Running => {
-                next_simulation_state.set(SimulationState::Paused);
+                pause_simulation(next_simulation_state);
             }
             SimulationState::Paused => {
-                next_simulation_state.set(SimulationState::Running);
+                resume_simulation(next_simulation_state);
             }
         }
     }
